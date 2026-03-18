b2c157ab8576f455a20946aebc8d9992a65810c2
goldenstein64 <goldnstein64@gmail.com>
2026-03-18 14:47:17 -0400

    build: move options to tlconfig

--- tlconfig.lua
+++ tlconfig.lua
@@ -1,6 +1,7 @@
 return {
-    build_dir = "build",
-    source_dir = "src",
-    include_dir = { "src", "types" },
-    gen_target = "5.1",
+   build_dir = "build",
+   source_dir = "src",
+   include_dir = { "src", "types" },
+   gen_target = "5.4",
+   gen_compat = "off",
 }
